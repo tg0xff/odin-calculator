de0079435d50e1c40b4648a8c37d23ae18cbe722
tg0xff <tg0xff@proton.me>
2024-04-21 21:23:50 -0600

    Rearrange operations object

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let operand0 = "";
 let operator = "";
 let operand1 = "";
+let screenContent = "0";
+let hasDecimalFraction = false;
 let operationToFunction = {
   "+": add,
   "−": subtract,
   "×": multiply,
   "÷": divide,
 };
-let screenContent = "0";
-let hasDecimalFraction = false;
 
 const calcInputs = document.querySelector("#calc-inputs");
 calcInputs.addEventListener("click", main);
