52b66a07b5c2cd4cfba7ca4f51cc6114bae25da8
tg0xff <tg0xff@proton.me>
2024-04-23 15:53:59 -0600

    Sync colourscheme with browser preference

--- colour.js
+++ colour.js
@@ -1,3 +1,14 @@
 const html = document.querySelector("html");
 const colourButton = document.querySelector("#colourscheme");
-colourButton.addEventListener("click", () => html.classList.toggle("dark-mode"));
+colourButton.addEventListener("click", () =>
+  html.classList.toggle("dark-mode"),
+);
+
+const prefersColor = window.matchMedia("(prefers-color-scheme: dark)");
+prefersColor.addEventListener("change", (e) => {
+  if (e.matches) {
+    html.classList.add("dark-mode");
+  } else {
+    html.classList.remove("dark-mode");
+  }
+});
